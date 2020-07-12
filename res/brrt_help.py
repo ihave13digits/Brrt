49ff0a8566b4b071dd2b9b3b9e75c1f88a470399
ihave13digits <ihave13digits@protonmail.com>
2020-07-12 15:02:07 -0400

    updated resources

---
+++ res/brrt_help.py
@@ -0,0 +1,36 @@
+helper = {
+        ### All ###
+        '' : 'banter, banter <>\npraise, praise <>\nd <>\n',
+        ### Misc. ###
+        
+        # Banter
+        'banter' : "",
+        'banter <>' : "",
+        # Praise
+        'praise' : "",
+        'praise <>' : "",
+        # Roll Die
+        'd <>' : "",
+
+        ### Docs. ###
+
+        # Source
+        'source' : "",
+        'source guts' : "",
+        'source read' : "",
+        'source tools' : "",
+        # Python
+        'python' : "",
+        'python index' : "",
+        'python module' : "",
+        'python glossary' : "",
+        # Rust
+        'rust' : "",
+        'rust mod' : "",
+        'rust error' : "",
+        'rust rustc' : "",
+        'rust edition' : "",
+        'rust rustdoc' : "",
+        'rust foreword' : "",
+        'rust standard' : "",
+        }
