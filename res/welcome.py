c1f71e28e8d80755310a1d9dd78646c6fa0c185b
ihave13digits <ihave13digits@protonmail.com>
2020-07-16 19:06:13 -0400

    added resource welcome.py

---
+++ res/welcome.py
@@ -0,0 +1,34 @@
+messages = {
+        'sm' : [
+            # server, member
+            'Welcome to {}, {}!',
+            "Look out, {}!  {} just joined!",
+            "{} just gained an ally called {}!",
+            "{} welcomes you, {}!",
+            "{} hopes you enjoy your stay, {}!",
+            "Look who just joined {}!  Welcome{}!",
+            "{} just got a new member!  Come and introduce yourself, {}!",
+            ],
+        'ms' : [
+            # server, member
+            '{} joined {}!',
+            "{} just joined {}!",
+            "{} has discovered{}!",
+            "I found {} in {}!",
+            ],
+        'm' : [
+            # member
+            'Welcome, {}!',
+            "{} just joined!",
+            "We now have {}!",
+            "Nice to meet you, {}!  Care to introduce yourself?",
+            "Hello {}!  Welcome to the club!",
+            "{} joined and this makes Brrt happy!",
+            "If anyone was wasn't aware, {} just joined!",
+            "{}!  Welcome!",
+            "Welcome to the group, {}!",
+            "Welcome to the server, {}!",
+            ]
+        }
+
+welcome_types = ['sm', 'ms', 'm']
