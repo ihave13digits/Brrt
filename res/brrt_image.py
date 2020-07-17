69f45f508bc7155844fa8caed3bdc1afca5c01fd
ihave13digits <ihave13digits@protonmail.com>
2020-07-17 19:17:11 -0400

    added resource

---
+++ res/brrt_image.py
@@ -0,0 +1,7 @@
+image = {
+        'brrt' : "https://raw.githubusercontent.com/ihave13digits/Brrt/master/img/Brrt.png",
+        'brrt_mail' : "https://raw.githubusercontent.com/ihave13digits/Brrt/master/img/BrrtMiniMail.png",
+        'brrt_coin_mini' : "https://raw.githubusercontent.com/ihave13digits/Brrt/master/img/BrrtMiniCoin.png",
+        'brrt_coin_tails' : "https://raw.githubusercontent.com/ihave13digits/Brrt/master/img/BrrtCoinTails.png",
+        'brrt_coin_heads' : "https://raw.githubusercontent.com/ihave13digits/Brrt/master/img/BrrtCoinHeads.png",
+        }
