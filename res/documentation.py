4342ca93b82c6c8f10d364b45f38c3e80f225e2c
ihave13digits <ihave13digits@protonmail.com>
2020-07-20 17:16:59 -0400

    updated documentation

--- res/documentation.py
+++ res/documentation.py
@@ -1,89 +1,80 @@
-source = {
-        '' : 'https://github.com/ihave13digits/Brrt',
-        'guts' : 'https://github.com/ihave13digits/Brrt/blob/master/bot.py',
-        'read' : 'https://github.com/ihave13digits/Brrt/blob/master/README.md',
-        'tools' : 'https://github.com/ihave13digits/Brrt/blob/master/tools.py',
-        }
+docs = {
+        'source' : {
+            '' : 'https://github.com/ihave13digits/Brrt',
+            'guts' : 'https://github.com/ihave13digits/Brrt/blob/master/bot.py',
+            'read' : 'https://github.com/ihave13digits/Brrt/blob/master/README.md',
+            'tools' : 'https://github.com/ihave13digits/Brrt/blob/master/tools.py',
+            },
+        'c' : {
+            '' : 'https://www.cprogramming.com/',
+            'function' : 'https://www.cprogramming.com/function.html?inl=nv',
+            'syntax' : 'https://www.cprogramming.com/reference/?inl=nv',
+            },
+        'c_sharp' : {
+            '' : 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/',
+            'keyword' : 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/',
+            'operator' : 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/',
+            'token' : 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/tokens/',
+            'preprocessor' : 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/preprocessor-directives/',
+            'options' : 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/compiler-options/',
+            'errors' : 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/compiler-messages/',
+            },
+        'c_pp' : {
+            '' : 'http://www.cplusplus.com/doc/',
+            },
 
-c = {
-        '' : 'https://www.cprogramming.com/',
-        'function' : 'https://www.cprogramming.com/function.html?inl=nv',
-        'syntax' : 'https://www.cprogramming.com/reference/?inl=nv',
-        }
-
-c_sharp = {
-        '' : 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/',
-        'keyword' : 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/',
-        'operator' : 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/',
-        'token' : 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/tokens/',
-        'preprocessor' : 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/preprocessor-directives/',
-        'options' : 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/compiler-options/',
-        'errors' : 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/compiler-messages/',
-        }
-
-c_pp = {
-        '' : 'http://www.cplusplus.com/doc/',
-        }
-
-java = {
-         '' : 'https://docs.oracle.com/en/java/javase/14/docs/api/index.html',
-         'classes' : 'https://docs.oracle.com/en/java/javase/14/docs/api/allclasses-index.html',
-         'packages' : 'https://docs.oracle.com/en/java/javase/14/docs/api/allpackages-index.html',
-         'properties' : 'https://docs.oracle.com/en/java/javase/14/docs/api/system-properties.html',
-         }
-
-javascript = {
-         '' : 'https://devdocs.io/javascript/',
-         }
-
-lua = {
-         '' : 'https://www.lua.org/docs.html',
-         'intro' : 'https://www.lua.org/manual/5.4/manual.html',
-         'basic' : 'https://www.lua.org/manual/5.4/manual.html#2',
-         'language' : 'https://www.lua.org/manual/5.4/manual.html#3',
-         'api' : 'https://www.lua.org/manual/5.4/manual.html#4',
-         'auxiliary' : 'https://www.lua.org/manual/5.4/manual.html#5',
-         'standard' : 'https://www.lua.org/manual/5.4/manual.html#6',
-         'standalone' : 'https://www.lua.org/manual/5.4/manual.html#7',
-         'version-issues' : 'https://www.lua.org/manual/5.4/manual.html#8',
-         'syntax' : 'https://www.lua.org/manual/5.4/manual.html#9',
-         }
-
-perl = {
-         '' : 'https://www.perl.org/docs.html',
-         'manual' : 'https://perldoc.perl.org/perl.html',
-         'module' : 'https://perldoc.perl.org/index-modules-A.html',
-         }
-
-python = {
-        '' : 'https://docs.python.org/3/',
-        'index' : 'https://docs.python.org/3/genindex.html',
-        'module' : 'https://docs.python.org/3/py-modindex.html',
-        'glossary' : 'https://docs.python.org/3/glossary.html',
-        
-        }
-
-ruby = {
-         '' : 'https://ruby-doc.org/',
-         'api' : 'https://ruby-doc.org/core-2.7.1/',
-         'standard' : 'https://ruby-doc.org/stdlib-2.7.1/',
-         }
-
-rust = {
-        '' : 'https://www.rust-lang.org/learn',
-        'edition' : 'https://doc.rust-lang.org/edition-guide/index.html',
-        'foreword' : 'https://doc.rust-lang.org/stable/book/foreword.html',
-        'standard' : 'https://doc.rust-lang.org/std/index.html',
-        'rustdoc' : 'https://doc.rust-lang.org/rustdoc/index.html',
-        'rustc' : 'https://doc.rust-lang.org/rustc/index.html',
-        'error' : 'https://doc.rust-lang.org/error-index.html',
-        'mod' : 'https://doc.rust-lang.org/book/ch07-02-defining-modules-to-control-scope-and-privacy.html',
-        }
-
-api = {
-        '' : "Brrt doesn't take that argument, knucklehead!  Try using the command `!api`.",
-        'discord' : 'https://discordpy.readthedocs.io/en/latest/',
-        'godot' : 'https://docs.godotengine.org/en/stable/',
-        'unity' : 'https://docs.unity3d.com/Manual/',
-        'unreal' : 'https://docs.unrealengine.com/en-US/index.html',
-        }
+        'java' : {
+            '' : 'https://docs.oracle.com/en/java/javase/14/docs/api/index.html',
+            'classes' : 'https://docs.oracle.com/en/java/javase/14/docs/api/allclasses-index.html',
+            'packages' : 'https://docs.oracle.com/en/java/javase/14/docs/api/allpackages-index.html',
+            'properties' : 'https://docs.oracle.com/en/java/javase/14/docs/api/system-properties.html',
+            },
+        'javascript' : {
+            '' : 'https://devdocs.io/javascript/',
+            },
+        'lua' : {
+            '' : 'https://www.lua.org/docs.html',
+            'intro' : 'https://www.lua.org/manual/5.4/manual.html',
+            'basic' : 'https://www.lua.org/manual/5.4/manual.html#2',
+            'language' : 'https://www.lua.org/manual/5.4/manual.html#3',
+            'api' : 'https://www.lua.org/manual/5.4/manual.html#4',
+            'auxiliary' : 'https://www.lua.org/manual/5.4/manual.html#5',
+            'standard' : 'https://www.lua.org/manual/5.4/manual.html#6',
+            'standalone' : 'https://www.lua.org/manual/5.4/manual.html#7',
+            'version-issues' : 'https://www.lua.org/manual/5.4/manual.html#8',
+            'syntax' : 'https://www.lua.org/manual/5.4/manual.html#9',
+            },
+        'perl' : {
+            '' : 'https://www.perl.org/docs.html',
+            'manual' : 'https://perldoc.perl.org/perl.html',
+            'module' : 'https://perldoc.perl.org/index-modules-A.html',
+            },
+        'python' : {
+            '' : 'https://docs.python.org/3/',
+            'index' : 'https://docs.python.org/3/genindex.html',
+            'module' : 'https://docs.python.org/3/py-modindex.html',
+            'glossary' : 'https://docs.python.org/3/glossary.html',
+            },
+        'ruby' : {
+            '' : 'https://ruby-doc.org/',
+            'api' : 'https://ruby-doc.org/core-2.7.1/',
+            'standard' : 'https://ruby-doc.org/stdlib-2.7.1/',
+            },
+        'rust' : {
+            '' : 'https://www.rust-lang.org/learn',
+            'edition' : 'https://doc.rust-lang.org/edition-guide/index.html',
+            'foreword' : 'https://doc.rust-lang.org/stable/book/foreword.html',
+            'standard' : 'https://doc.rust-lang.org/std/index.html',
+            'rustdoc' : 'https://doc.rust-lang.org/rustdoc/index.html',
+            'rustc' : 'https://doc.rust-lang.org/rustc/index.html',
+            'error' : 'https://doc.rust-lang.org/error-index.html',
+            'mod' : 'https://doc.rust-lang.org/book/ch07-02-defining-modules-to-control-scope-and-privacy.html',
+            },
+        'api' : {
+            '' : "Brrt doesn't take that argument, knucklehead!  Try using the command `!api`.",
+            'discord' : 'https://discordpy.readthedocs.io/en/latest/',
+            'godot' : 'https://docs.godotengine.org/en/stable/',
+            'unity' : 'https://docs.unity3d.com/Manual/',
+            'unreal' : 'https://docs.unrealengine.com/en-US/index.html',
+            }
+    }
